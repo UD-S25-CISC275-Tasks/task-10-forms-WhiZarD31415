631fed8d9e42241ecb061d428d61684fde15b326
Connor Vitz <connorvitz@gmail.com>
2025-03-15 13:24:16 -0400

    Formatting CheckAnswer and finish GiveAttempts

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -7,14 +7,14 @@ export function CheckAnswer({
     expectedAnswer: string;
 }): React.JSX.Element {
     const [answer, setAnswer] = useState<string>("");
-    // This is the Control
+
     function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
         setAnswer(event.target.value);
     }
 
-    // This is the View
     return (
         <div>
+            <h3>Check Answer</h3>
             <Form.Group controlId="formCheckAnswer">
                 <Form.Label>Your Answer:</Form.Label>
                 <Form.Control value={answer} onChange={updateAnswer} />
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [request, setRequest] = useState<number>(0);
+
+    function updateRequest(event: React.ChangeEvent<HTMLInputElement>) {
+        const numRequest: number = parseInt(event.target.value) || 0;
+        setRequest(numRequest);
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts Remaining: {attempts}</div>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Attempts to Gain:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={request}
+                    onChange={updateRequest}
+                />
+            </Form.Group>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts - 1);
+                }}
+                disabled={attempts === 0}
+            >
+                Use
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + request);
+                }}
+            >
+                Gain
+            </Button>
         </div>
     );
 }
